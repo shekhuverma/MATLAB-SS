f3e0a1f5535988a27f8f421a3d5160a26eb56d00
Shekhar verma <shekharvrm47@gmail.com>
2017-09-06 14:57:20 +0530

    Add files via upload

--- exp_6.m
+++ exp_6.m
@@ -11,7 +11,7 @@ for i=1:length(t)
     end
 end
 x=[zeros(1,min) 2 2 -1 3 7 1 2 -3 zeros(1,max)];
-subplot(1,3,1);
+subplot(1,4,1);
 stem(x);
 for i=1:length(t)
     if t(i)==-2
@@ -22,8 +22,11 @@ for i=1:length(t)
     end
 end
 y=[zeros(1,min) 1 -1 2 -2 4 1 -2 5 zeros(1,max)];
-subplot(1,3,2);
+subplot(1,4,2);
 stem(y);
 r=xcorr(x,y);
-subplot(1,3,3);
+r1=xcorr(y,x);
+subplot(1,4,3);
 stem(r);
+subplot(1,4,4);
+stem(r1);
